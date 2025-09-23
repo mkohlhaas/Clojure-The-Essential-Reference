e7eb98a7b1af8e8382aa7cb985f55e69cd2ffbaf
Hans-J. Schmid <knock@myopendoor.de>
2025-09-23 19:56:20 +0200

    2.3.2 ->>

--- 02-CreatingAndManipulatingFunctions/3-ThreadingMacros/2 - ->>/4.clj
+++
@@ -1,5 +0,0 @@
-(defn params [query] ; <1>
-  (->> (split query #"&")
-       (map #(split % #"="))
-       (map #(apply hash-map %))
-       (apply merge)))
---
+++ 02-CreatingAndManipulatingFunctions/3-ThreadingMacros/2 - ->>/f/src/f/core.clj
@@ -0,0 +1,37 @@
+(ns f.core
+  (:require [clojure.string :refer [split]]))
+
+;; Thread Last Macro
+
+(filter pos?
+        (filter #(apply = (str %))
+                (filter #(zero? (mod % 3))
+                        (filter even? (range 1000)))))
+; (6 66 222 444 666 888)
+
+(->> (range 1000)
+     (filter even?)
+     (filter #(zero? (mod % 3)))
+     (filter #(apply = (str %)))
+     (filter pos?))
+; (6 66 222 444 666 888)
+
+(def sample-query "guidx=123&flip=true")
+
+(defn params1 [query]
+  (apply merge                            ; {"guidx" "123", "flip" "true"}
+         (map #(apply hash-map %)         ; ({"guidx" "123"} {"flip" "true"})
+              (map #(split % #"=")        ; (["guidx" "123"] ["flip" "true"])
+                   (split query #"&"))))) ; ["guidx=123" "flip=true"]
+
+(params1 sample-query)
+; {"guidx" "123", "flip" "true"}
+
+(defn params2 [query]
+  (->> (split query #"&")        ; ["guidx=123" "flip=true"]
+       (map #(split % #"="))     ; (["guidx" "123"] ["flip" "true"])
+       (map #(apply hash-map %)) ; ({"guidx" "123"} {"flip" "true"})
+       (apply merge)))           ; {"guidx" "123", "flip" "true"}
+
+(params2 sample-query)
+; {"guidx" "123", "flip" "true"}
