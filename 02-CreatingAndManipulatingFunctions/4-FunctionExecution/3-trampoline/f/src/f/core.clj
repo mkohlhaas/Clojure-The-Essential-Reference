646f88c1f80da70467cee78b1380dc1c39c8c291
Hans-J. Schmid <knock@myopendoor.de>
2025-09-25 11:25:41 +0200

    2.4.3 trampoline

--- 02-CreatingAndManipulatingFunctions/4-FunctionExecution/3-trampoline/f/src/f/core.clj
+++ 02-CreatingAndManipulatingFunctions/4-FunctionExecution/3-trampoline/f/src/f/core.clj
@@ -1,40 +1,50 @@
-(defn- invoke                                     ; <1>
+(ns f.core)
+
+(defn- invoke
   [f-key & args]
   (apply (resolve (symbol (name f-key))) args))
 
-(defn green [[light & lights]]                    ; <2>
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
+(defn green [[light & lights]]
   #(case light
      :red false
-     nil true
+     nil  true
      (invoke light lights)))
 
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn red [[light & lights]]
   #(case light
      :amber false
      nil true
      (invoke light lights)))
 
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn amber [[light & lights]]
   #(case light
      :green false
      nil true
      (invoke light lights)))
 
-(defn flashing-red [[light & lights]]             ; <3>
-  #(if (nil? light) true (invoke light lights)))
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
+(defn flashing-red [[light & lights]]
+  #(if (nil? light)
+     true
+     (invoke light lights)))
 
 (defn flashing-amber [[light & lights]]
-  #(if (nil? light) true (invoke light lights)))
+  #(if (nil? light)
+     true
+     (invoke light lights)))
 
-(defn traffic-light [lights]                      ; <4>
+(defn traffic-light [lights]
   (trampoline flashing-amber lights))
 
 (traffic-light [:red :amber :red])
-;; false
+; false
 
 (traffic-light [:red :green :amber :red])
-;; true
+; true
 
 (time (traffic-light (take 10000000 (cycle [:amber :red :green]))))
-;; "Elapsed time: 5919.991775 msecs"
-;;true
+; (out) "Elapsed time: 11418.777125 msecs"
+; true
--- 02-CreatingAndManipulatingFunctions/4-FunctionExecution/trampoline/1.clj
+++
@@ -1,3 +0,0 @@
-(trampoline
-  ([f])
-  ([f & args]))
