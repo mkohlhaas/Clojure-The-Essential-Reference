46c9bfd3b9339f41e3e43c8453bcfe31e06df8d6
Hans-J. Schmid <knock@myopendoor.de>
2025-11-04 15:21:14 +0100

    8.3.7 replace

--- 08 - Collections/3 - General Purpose/7 - replace/f/src/f/core.clj
+++ 08 - Collections/3 - General Purpose/7 - replace/f/src/f/core.clj
@@ -3,6 +3,7 @@
    [clojure.string]
    [criterium.core :refer [quick-bench]]))
 
+;; uses a substitution map (any associative data structure - vectors, maps, …)
 (replace {:a "a" :b "b"} [:a 1 2 :b 3 4])
 ; ["a" 1 2 "b" 3 4]
 
@@ -13,16 +14,36 @@
  ["0" 1 2 "0" 10 11])
 ; 30
 
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+;; vector as substitution map uses the index of the vector to match the element to replace
+;; creates this subsitution map: {0 :a 1 :b 2 :c}
 (replace [:a :b :c] (range 10))
 ; (:a :b :c 3 4 5 6 7 8 9)
 
-(defn entry [k v] (clojure.lang.MapEntry/create k v))
+;; replace key-value pairs in maps (in practice a less common operation)
 
-(def user {:name "jack" :city "London" :id 123})
-(def sub  {(entry :city "London") [:postcode "WD12"]})
+;; when a map is iterated sequentially, it returns a list of MapEntry objects, which is what we need to match against
+(defn entry [k v]
+  (clojure.lang.MapEntry/create k v))
 
+(comment
+  (entry :city "London")         ; [:city "London"]
+  (type (entry :city "London"))) ; clojure.lang.MapEntry
+
+;; replace :city with :postcode (!!!)
+(def sub  {(entry :city "London") [:postcode "WD12"]}) ; {[:city "London"] [:postcode "WD12"]}    
+(def user {:name "jack" :city "London" :id 123})       ; {:name "jack", :city "London", :id 123}
+
+;; city has been replaced by its postcode
 (into {} (replace sub user))
 ; {:name "jack", :postcode "WD12", :id 123}
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Textual Substitution System (Templates)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def text "You provided the following: user {usr} password {pwd}")
 (def sub1 {"{usr}" "'rb075'" "{pwd}" "'xfrDDjsk'"})
@@ -32,11 +53,17 @@
   (replace sub1)
   (interpose " "))
  str
- (clojure.string/split text #"\s"))
+ (clojure.string/split text #"\s")) ; ["You" "provided" "the" "following:" "user" "{usr}" "password" "{pwd}"]
 ; "You provided the following: user 'rb075' password 'xfrDDjsk'"
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (comment
   (defn large-map [i] (into {} (map vector (range i) (range i))))
+
+  (large-map 10) ; {0 0, 7 7, 1 1, 4 4, 6 6, 3 3, 2 2, 9 9, 5 5, 8 8}
 
   (def big-map (large-map 2e6))
 
