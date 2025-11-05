28be0d1651c4ea27b5bd6a90f061121d3c1db45c
Hans-J. Schmid <knock@myopendoor.de>
2025-11-05 16:21:11 +0100

    8.4.3 prewalk-replace and postwalk-replace

--- 08 - Collections/4 - Traversing/3 - prewalk-replace and postwalk-replace/f/src/f/core.clj
+++ 08 - Collections/4 - Traversing/3 - prewalk-replace and postwalk-replace/f/src/f/core.clj
@@ -1,5 +1,6 @@
 (ns f.core
-  (:require [clojure.walk :refer [prewalk-replace postwalk-replace]]))
+  (:require
+   [clojure.walk :refer [postwalk-replace prewalk-replace]]))
 
 (def data
   [[1 2]
@@ -7,13 +8,33 @@
    [:d 14]])
 
 (prewalk-replace {:a "A" :b "B" :c "C" :d "D"} data)
-; [[1 2] [3 "A" [5 [6 7 "B" [] 9] 10 [11 "C"]]] ["D" 14]]
+; [[1 2] 
+; [3 "A" [5 [6 7 "B" [] 9] 10 [11 "C"]]] 
+; ["D" 14]
 
+;; ;;;;;;;;;;;;;
+;; Greek Letters
+;; ;;;;;;;;;;;;;
+
+;; substitution map can be any data structure supporting `contains?`
+;; e.g. array-map, hash-map, but also vector
+
+;; vector as substitution map (using indices)
 (def ^:const greek '[α β γ δ ε ζ η θ ι κ λ μ ν ξ ο π ρ σ τ υ φ χ ψ ω])
 
-(prewalk-replace greek data)
-; [[β γ] [δ :a [ζ [η θ :b [] κ] λ [μ :c]]] [:d ο]]
+;; Greek alphabet has 24 letters
+(count greek) ; 24
 
+(prewalk-replace greek data)
+; [[β γ] 
+; [δ :a [ζ [η θ :b [] κ] λ [μ :c]]] 
+; [:d ο]
+
+;; ;;;;;;;;;;;;;;;;;;;
+;; Boolean Expressions
+;; ;;;;;;;;;;;;;;;;;;;
+
+;; Boolean formula of 16 variables
 (def formula
   '(and (and a1 a2)
         (or (and a16 a3) (or a5 a8)
@@ -24,10 +45,12 @@
                   (or (and a1 a4) a14
                       (and a15 a16))))))
 
+;; truth table for and
 (def ands
   '{(and true  true) true  (and true  false) false
     (and false true) false (and false false) false})
 
+;; truth table for or
 (def ors
   '{(or true  true) true (or true  false) true
     (or false true) true (or false false) false})
@@ -38,9 +61,11 @@
     a9  false a10 false a11 true  a12 false
     a13 true  a14 true  a15 true  a16 false})
 
+;; first replace the vars, then use truth tables
 (def transformed-formula
   (postwalk-replace (merge var-map ands ors) formula))
 
+;; simplified formula (but not final)
 transformed-formula
 ; (and
 ;  false
