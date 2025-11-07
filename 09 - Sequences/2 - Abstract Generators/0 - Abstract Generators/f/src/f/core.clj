c606e3eb15c20186a76ee60b782e2b8b79a6347f
Hans-J. Schmid <knock@myopendoor.de>
2025-11-07 22:05:38 +0100

    9.2 Abstract Generators

--- 09 - Sequences/2 - Abstract Generators/0 - Abstract Generators/f/src/f/core.clj
+++ 09 - Sequences/2 - Abstract Generators/0 - Abstract Generators/f/src/f/core.clj
@@ -1,18 +1,20 @@
 (ns f.core)
 
-#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
+;; ;;;;;;;;;;;;;;;;;;;;
+;; Implementation Notes
+;; ;;;;;;;;;;;;;;;;;;;;
+
+;; with `cons` and `lazy-seq`
+
 (defn repeatedly* [f]
   (lazy-seq (cons (f) (repeatedly* f))))
 
-#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn iterate* [f x]
   (lazy-seq (cons x (iterate* f (f x)))))
 
-#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn repeat* [x]
   (lazy-seq (cons x (repeat* x))))
 
-#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn cycle* [coll]
   ((fn step [[x & xs]]
      (lazy-seq
@@ -20,9 +22,14 @@
         (cons x (step xs))
         (cycle* coll)))) coll))
 
-#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (defn range* [n]
   ((fn step [x]
      (lazy-seq
       (when (< x n)
         (cons x (step (inc x)))))) 0))
+
+(take 10 (repeatedly* #(inc 0))) ; (1 1 1 1 1 1 1 1 1 1)
+(take 10 (iterate* inc 0))       ; (0 1 2 3 4 5 6 7 8 9)
+(take 10 (repeat* 1))            ; (1 1 1 1 1 1 1 1 1 1)
+(take 10 (cycle* '(1 2 3)))      ; (1 2 3 1 2 3 1 2 3 1)
+(range* 10)                      ; (0 1 2 3 4 5 6 7 8 9)
