2ec4fcfc9b0d7e411e90dcdf8e1f2e8152c37910
Hans-J. Schmid <knock@myopendoor.de>
2025-11-08 19:21:37 +0100

    9.3.5 re-seq

--- 09 - Sequences/3 - Other Generators/5 - re-seq/f/src/f/core.clj
+++ 09 - Sequences/3 - Other Generators/5 - re-seq/f/src/f/core.clj
@@ -10,14 +10,20 @@
 (def sb (doto (StringBuilder.)
           (.append "23")
           (.append "aa 42")))
+; #object[java.lang.StringBuilder 0x16ef8c13 "23aa 42"]
 
+;; also works with StringBuilders
 (re-seq #"\d+" sb)                               ; ("23" "42")
 
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
 (seq "hello")                                    ; (\h \e \l \l \o)
-
 (map (memfn toUpperCase) (map str "hello"))      ; ("H" "E" "L" "L" "O")
-
 (map (memfn toUpperCase) (re-seq #"\w" "hello")) ; ("H" "E" "L" "L" "O")
+
+ ;; Teach Programming to a Group of Kids
 
 (def signed-up
   "Jack 221 610-5007 (call after 9pm),
@@ -27,13 +33,25 @@
    Jane 661 773-8656,
    Ron 555 515-0158")
 
+(comment
+  (re-seq #"(\w+) (\d{3}) \d{3}-\d{4}" signed-up))
+; (["Jack 221 610-5007" "Jack" "221"]
+;  ["Anna 221 433-4185" "Anna" "221"]
+;  ["Charlie 661 471-3948" "Charlie" "661"]
+;  ["Hugo 661 653-4480" "Hugo" "661"]
+;  ["Jane 661 773-8656" "Jane" "661"]
+;  ["Ron 555 515-0158" "Ron" "555"])
+
 (let [people (re-seq #"(\w+) (\d{3}) \d{3}-\d{4}" signed-up)]
-  {:names (map second people) :area (map last people)})
+  {:names (map second people) :areas (map last people)})
 ; {:names ("Jack" "Anna" "Charlie" "Hugo" "Jane" "Ron"),
-;  :area ("221" "221" "661" "661" "661" "555")}
+;  :areas ("221" "221" "661" "661" "661" "555")}
+
+;; Book with a Million Digits of Pi
+(def ebook-of-pi "https://tinyurl.com/pi-digits")
 
 (comment
-  (def pi-digits (slurp "https://tinyurl.com/pi-digits"))
+  (def pi-digits (slurp ebook-of-pi))
 
   (def pi-seq
     (sequence
@@ -41,10 +59,14 @@
       cat
       (map int)
       (map #(mod % 48)))
-     (re-seq #"\d{10}" pi-digits)))
+     (re-seq #"\d{10}" pi-digits))) ;; ("1415926535" "8979323846" "2643383279" "5028841971" "6939937510" "5820974944"
 
   (take 20 pi-seq)) ; (1 4 1 5 9 2 6 5 3 5 8 9 7 9 3 2 3 8 4 6)
 
+;; Lazier than lazy (Making the book loading lazy, too.)
+
+;; The sequence generation downloads just enough HTTP request to satisfy the number of digits to print, 
+;; preventing the entire book to reside in memory all at once.
 (defn restream-seq [^Pattern re ^InputStream is]
   (let [s (Scanner. is "UTF-8")]
     ((fn step []
@@ -60,7 +82,7 @@
     (map #(mod % 48)))
    (restream-seq #"\d{10}" is)))
 
-(def pi-digits (URL. "https://tinyurl.com/pi-digits"))
+(def pi-digits (URL. ebook-of-pi))
 
 (with-open [is (.openStream pi-digits)]
   (doall (take 20 (pi-seq-fn is))))
