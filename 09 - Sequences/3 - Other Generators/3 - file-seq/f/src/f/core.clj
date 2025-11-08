332717bf95bc9094c85a57d1a5e3b3cecd71ce81
Hans-J. Schmid <knock@myopendoor.de>
2025-11-08 17:38:55 +0100

    9.3.3 file-seq

--- 09 - Sequences/3 - Other Generators/3 - file-seq/f/src/f/core.clj
+++ 09 - Sequences/3 - Other Generators/3 - file-seq/f/src/f/core.clj
@@ -3,45 +3,48 @@
    [clojure.java.io :as io]
    [clojure.string :as str]))
 
+;; depth-first traversal
+
 (count (file-seq (io/file "/usr/share/man"))) ; 26446
 
 (->> (io/file "/etc")
      (file-seq)
      (map (memfn getPath))
-     (take 6))
+     (take 10))
 ; ("/etc"
 ;  "/etc/passwd-"
 ;  "/etc/vconsole.conf"
 ;  "/etc/environment"
 ;  "/etc/ostree-mkinitcpio.conf"
-;  "/etc/resolv.conf")
+;  "/etc/resolv.conf"
+;  "/etc/cifs-utils"
+;  "/etc/cifs-utils/idmap-plugin"
+;  "/etc/gprofng.rc"
+;  "/etc/depmod.d")
+
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
 
 (def work-dir      (file-seq (java.io.File. ".")))
 (def abstract-path (file-seq (java.io.File. "")))
 (def non-existent  (file-seq (java.io.File. "NONE")))
 
-(.getAbsolutePath (first work-dir))
-; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f/."
+(.getAbsolutePath (first work-dir))      ; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f/."
+(.getAbsolutePath (first abstract-path)) ; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f"
+(.getAbsolutePath (first non-existent))  ; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f/NONE"
 
-(count work-dir) ; 335
-
-(.getAbsolutePath (first abstract-path))
-; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f"
-
+(count work-dir)      ; 335
+(count abstract-path) ; 335
 (count abstract-path) ; 335
 
-(.getAbsolutePath (first non-existent))
-; "/home/schmidh/Gitrepos/Clojure-The-Essential-Reference/09 - Sequences/3 - Other Generators/3 - file-seq/f/NONE"
-
-(count abstract-path) ; 335
-
-(defn grep-by-type [q ext]
+(defn grep-by-type [query ext]
   (sequence
    (comp
     (remove (memfn isDirectory))
-    (map (memfn getAbsolutePath))
+    (map    (memfn getAbsolutePath))
     (filter #(= ext (last (str/split % #"\."))))
-    (filter #(str/includes? (slurp %) q)))
+    (filter #(str/includes? (slurp %) query)))
    (file-seq (java.io.File. "."))))
 
 (grep-by-type "file-seq" "clj")
