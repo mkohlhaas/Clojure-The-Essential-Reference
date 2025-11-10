bf059a5e2a35f62e6a93118cae08896beec7a65c
Hans-J. Schmid <knock@myopendoor.de>
2025-11-10 11:33:33 +0100

    9.4.1 list

--- 09 - Sequences/4 - Lists/1 - list/f/src/f/core.clj
+++ 09 - Sequences/4 - Lists/1 - list/f/src/f/core.clj
@@ -1,9 +1,19 @@
 (ns f.core
   (:require
-   [clojure.set :refer [map-invert]]
+   [clojure.set    :refer [map-invert]]
    [criterium.core :refer [quick-bench]]))
 
+;; lists are not lazy
+
 (list 1 2 3 4 5) ; (1 2 3 4 5)
+
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+(type ())          ; clojure.lang.PersistentList$EmptyList
+(type (list))      ; clojure.lang.PersistentList$EmptyList
+(type (cons 1 ())) ; clojure.lang.Cons
 
 (-> () (conj 1))                    ; (1)
 (-> () (conj 1) (conj 2))           ; (2 1)
@@ -13,15 +23,19 @@
   (into () coll))
 
 (rev (range 10))  ; (9 8 7 6 5 4 3 2 1 0)
+(rev [0 1 2 3])   ; (3 2 1 0)
 
-(defn stack [] ())
-(defn push [x stack] (conj stack x))
+(defn stack []
+  ())
+
+(defn push [x stack]
+  (conj stack x))
 
 (defn nearest-smaller [xs]
-  (letfn [(step [xs st]
+  (letfn [(step [xs stack]
             (lazy-seq
              (when-first [x xs]
-               (loop [st st]
+               (loop [st stack]
                  (if-let [s (peek st)]
                    (if (< s x)
                      (cons s (step (rest xs) (push x st)))
@@ -43,26 +57,26 @@
   (defn acons [n]
     (reduce #(cons %2 %1) () (range n)))
 
-  (let [l1 (alist 1e5)] (quick-bench (reduce + l1)))
-  (let [l2 (acons 1e5)] (quick-bench (reduce + l2)))
+  (let [l1 (alist 1e5)] (quick-bench (reduce + l1))) ; (out) Execution time mean :  5.452296 ms
+  (let [l2 (acons 1e5)] (quick-bench (reduce + l2))) ; (out) Execution time mean : 11.085810 ms
 
-  (let [l1 (alist 1e5)] (quick-bench (count l1)))
-  (let [l2 (acons 1e5)] (quick-bench (count l2))))
+  (let [l1 (alist 1e5)] (quick-bench (count l1)))    ; (out) Execution time mean : 15.737950 ns
+  (let [l2 (acons 1e5)] (quick-bench (count l2))))   ; (out) Execution time mean :  2.414748 ms
 
 (def push-me conj)
 (def brackets {\[ \] \( \) \{ \}})
 
 (defn check [form stack]
-  (reduce (fn [q x]
-            (cond
-              (brackets x) (push-me q x)
-              ((map-invert brackets) x)
-              (if (= (brackets (peek q)) x)
-                (pop q)
-                (throw
-                 (ex-info
-                  (str "Unmatched delimiter " x) {})))
-              :else q)) stack form))
+  (reduce
+   (fn [q x]
+     (cond
+       (brackets x)               (push-me q x)
+       ((map-invert brackets) x)  (if (= (brackets (peek q)) x)
+                                    (pop q)
+                                    (throw (ex-info (str "Unmatched delimiter " x) {})))
+       :else q))
+   stack
+   form))
 
 (comment
   (check "(let [a (inc 1]) (+ a 2))" ()))
@@ -79,8 +93,8 @@
   (def small (str (seq (take 100  (iterate list ())))))
   (def large (str (seq (take 1000 (iterate list ())))))
 
-  (quick-bench (check small ()))
-  (quick-bench (check small []))
+  (quick-bench (check small ()))  ; (out) Execution time mean : 3.938575 ms
+  (quick-bench (check small []))  ; (out) Execution time mean : 4.717053 ms
 
-  (quick-bench (check large ()))
-  (quick-bench (check large [])))
+  (quick-bench (check large ()))  ; (out) Execution time mean : 388.989038 ms
+  (quick-bench (check large []))) ; (out) Execution time mean : 471.480527 ms
