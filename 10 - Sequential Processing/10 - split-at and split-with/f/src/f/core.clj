4701c6f8b7b245d6bd11281633d3fd1575b5a832
Hans-J. Schmid <knock@myopendoor.de>
2025-11-13 18:05:44 +0100

    10.10 split-at and split-with

--- 10 - Sequential Processing/10 - split-at and split-with/f/src/f/core.clj
+++ 10 - Sequential Processing/10 - split-at and split-with/f/src/f/core.clj
@@ -1,28 +1,51 @@
 (ns f.core)
 
-(split-at 8 (range 10))
-; [(0 1 2 3 4 5 6 7) (8 9)]
+;; split at index     (`split-at`) 
+;; split at predicate (`split-with`)
 
-(split-with (complement zero?) [1 4 5 0 3 2 0 1 1 0])
-; [(1 4 5) (0 3 2 0 1 1 0)]
+;; they return two partitions
+
+(split-at 8 (range 10))                               ; [(0 1 2 3 4 5 6 7) (8 9)]
+
+;; split happens on the first false evaluation of the predicate
+(split-with (complement zero?) [1 4 5 0 3 2 0 1 1 0]) ; [(1 4 5) (0 3 2 0 1 1 0)]
+(split-with zero? [1 4 5 0 3 2 0 1 1 0])              ; [() (1 4 5 0 3 2 0 1 1 0)]
+(split-with zero? [0 0 0 1 4 5 0 3 2 0 1 1 0])        ; [(0 0 0) (1 4 5 0 3 2 0 1 1 0)]
 
 (let [[head others] (split-at 8 (range 10))]
   (+ (last head) (last others)))
 ; 16
 
-(take 10 (last (split-at 10 (range))))
-; (10 11 12 13 14 15 16 17 18 19)
+(comment
+  (split-at 8 (range 10))) ; [(0 1 2 3 4 5 6 7) (8 9)]
 
-(split-with (complement #{\a \e \i \o \u}) "hello")
-; [(\h) (\e \l \l \o)]
+(take 10 (last (split-at 10 (range)))) ; (10 11 12 13 14 15 16 17 18 19)
+
+(comment
+  (split-at 10 (range))) ; [(0 1 2 3 4 5 6 7 8 9) (10 11 12 13 14 15 16 17 18 19 …)]
+
+(split-with (complement #{\a \e \i \o \u}) "hello") ; [(\h) (\e \l \l \o)]
 
 (defn k?  [k]   (complement #{k}))
 (defn kv? [k v] (complement #{[k v]}))
 
-(split-with (k? 5)    (apply sorted-set (range 10))) ; [(0 1 2 3 4) (5 6 7 8 9)]
+(split-with (k? 5) (set (range 10)))              ; [(0 7 1 4 6 3 2 9) (5 8)]
+(split-with (k? 5) (apply sorted-set (range 10))) ; [(0 1 2 3 4) (5 6 7 8 9)]
+
+(comment
+  (set (range 10))               ; #{0 7 1 4 6 3 2 9 5 8}
+  (apply sorted-set (range 10))) ; #{0 1 2 3 4 5 6 7 8 9}
+
+(split-with (kv? 4 5) (apply hash-map   (range 10))) ; [([0 1]) ([4 5] [6 7] [2 3] [8 9])]
 (split-with (kv? 4 5) (apply sorted-map (range 10))) ; [([0 1] [2 3]) ([4 5] [6 7] [8 9])]
-(split-with (k? 5)    (set (range 10)))              ; [(0 7 1 4 6 3 2 9) (5 8)]
-(split-with (kv? 4 5) (apply hash-map (range 10)))   ; [([0 1]) ([4 5] [6 7] [2 3] [8 9])]
+
+(comment
+  (apply hash-map   (range 10))  ; {0 1, 4 5, 6 7, 2 3, 8 9}
+  (apply sorted-map (range 10))) ; {0 1, 2 3, 4 5, 6 7, 8 9}
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Multiple splits with `split-by`
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn split-by [pred coll]
   (lazy-seq
@@ -39,12 +62,18 @@
 
 (take 3 (split-by #(zero? (mod % 5)) (range))) ; ((0 1 2 3 4) (5 6 7 8 9) (10 11 12 13 14))
 
-(defn split-at-me [n coll]
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Implementations of `split-at` and `split-with`
+
+(defn split-at-n [n coll]
   [(take n coll) (drop n coll)])
 
-(split-at-me 8 (range 10)) ; [(0 1 2 3 4 5 6 7) (8 9)]
-
-(defn split-with-me [pred coll]
+(defn split-with-pred [pred coll]
   [(take-while pred coll) (drop-while pred coll)])
 
-(split-with-me (complement zero?) [1 4 5 0 3 2 0 1 1 0]) ; [(1 4 5) (0 3 2 0 1 1 0)]
+(split-at-n 8 (range 10))                                  ; [(0 1 2 3 4 5 6 7) (8 9)]
+
+(split-with-pred (complement zero?) [1 4 5 0 3 2 0 1 1 0]) ; [(1 4 5) (0 3 2 0 1 1 0)]
