b7b72392503a522aaa16765da8cd07f301b73f32
Hans-J. Schmid <knock@myopendoor.de>
2025-11-14 19:47:14 +0100

    10.11 when-first

--- 10 - Sequential Processing/11 - when-first/f/src/f/core.clj
+++ 10 - Sequential Processing/11 - when-first/f/src/f/core.clj
@@ -1,11 +1,21 @@
-(ns f.core
-  (:require [clojure.pprint]))
+(ns f.core)
 
-(when-first [x (range 10)] (str x))            ; "0"
+;; (when-first bindings <body>)
+;; "bindings" is a vector of exactly 2 elements
+
+(when-first [x (range 10)]
+  (str x))
+; "0"
 
 #_{:clj-kondo/ignore [:unused-binding]}
-(when-first [x ()] (print "never gets here")) ; nil
+(when-first [x ()]
+  (print "never gets here"))
 
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+;; chunks of 32 elements (although we need only the first)
 (first (map #(do (print ".") %) (range 100))) ; 0
 ; (out) ................................
 
@@ -15,44 +25,54 @@
      (cons x (dechunk (rest xs))))))
 
 (comment
+  ;; no chunks
   (first (map #(do (print ".") %) (dechunk (range 100)))))  ; 0
   ; (out) .
 
-(comment
-  (clojure.pprint/pprint
-   (macroexpand
-    '(when-first [x coll] (println x)))))
-  ; (out) (let*
-  ; (out)  [temp__5804 (seq coll)]
-  ; (out)  (when
-  ; (out)   temp__5804
-  ; (out)   (let  [xs__6360 temp__5804]
-  ; (out)    (let  [x (first xs__6360)]
-  ; (out)     (println x)))))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn take-first [coll]
+(comment
+  (macroexpand
+   '(when-first [x coll] (println x))))
+  ; (let*
+  ;  [temp__5804 (seq coll)] ; `seq` is called on "coll" with the effect of creating a sequential version (if it isn't’ already). It generates nil when "coll" is empty.
+  ;  (when
+  ;   temp__5804
+  ;   (let
+  ;    [xs__6360 temp__5804]
+  ;    (let
+  ;     [x (first xs__6360)]
+  ;     (println x)))))
+
+(defn take-first-1 [coll]
   (lazy-seq
    (when (seq coll)
      (cons (first coll) ()))))
 
-(comment
-  (take-first (sequence (map #(do (println "eval" %) %)) '(1)))) ; (1)
-  ; (out) eval 1
-
-(take-first (eduction (map #(do (println "eval" %) %)) '(1)))
-; eval 1
-; eval 1
+;; `sequence` is caching
+(take-first-1 (sequence (map #(do (println "eval" %) %)) '(1)))
+; (out) eval 1
 ; (1)
 
-(defn take-first-me [coll]
+;; `eduction` isn't caching
+;; `take-first` evaluates the input twice, once to check if it's empty and the second time to get the first element
+(take-first-1 (eduction (map #(do (println "eval" %) %)) '(1)))
+; (out) eval 1
+; (out) eval 1
+; (1)
+
+;; uses `when-first` which evaluates "coll" just once
+(defn take-first-2 [coll]
   (lazy-seq
    (when-first [x coll]
      (cons x ()))))
 
-(comment
-  (take-first-me (sequence (map #(do (println "eval" %) %)) '(1)))) ; (1)
-  ; (out) eval 1
+(take-first-2 (sequence (map #(do (println "eval" %) %)) '(1)))
+; (out) eval 1
+; (1)
 
-(take-first-me (eduction (map #(do (println "eval" %) %)) '(1)))
-; eval 1
+(take-first-2 (eduction (map #(do (println "eval" %) %)) '(1)))
+; (out) eval 1
 ; (1)
