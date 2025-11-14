2a027ba1e41eb6d0c81b9f7d5b0879e8dc25ce95
Hans-J. Schmid <knock@myopendoor.de>
2025-11-14 20:13:25 +0100

    10.12 chunk-cons, chunk-first, chunk-rest, chunk-next, chunk-buffer, chunk-append and chunk

--- 10 - Sequential Processing/12 - chunk-cons, chunk-first, chunk-rest, chunk-next, chunk-buffer, chunk-append and chunk/f/src/f/core.clj
+++ 10 - Sequential Processing/12 - chunk-cons, chunk-first, chunk-rest, chunk-next, chunk-buffer, chunk-append and chunk/f/src/f/core.clj
@@ -4,23 +4,32 @@
   (:import
    [java.io FileInputStream InputStream]))
 
-(def b (chunk-buffer 10))
+;; chunk-* functions were released as part of Clojure 1.1 and labeled as "implementation details" [173].
+;; The release note also specifies that they were made public to allow experimentation.
+;; Many years later, chunking functions are used extensively in the standard library and despite being still undocumented,
+;; there are no signs of them being deprecated, changed or removed.
+
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+(def b (chunk-buffer 10)) ; #object[clojure.lang.ChunkBuffer 0x3c3cebae "clojure.lang.ChunkBuffer@3c3cebae"]
 
 (chunk-append b 0) ; nil
 (chunk-append b 1) ; nil
 (chunk-append b 2) ; nil
 
-(def first-chunk (chunk b))
-
-(chunk-cons first-chunk ()) ; (0 1 2)
-
-#_{:clj-kondo/ignore [:redefined-var]}
-(def b (chunk-buffer 10))
-(chunk-append b 0) ; nil
-(chunk b)          ; #object[clojure.lang.ArrayChunk 0x76fe0355 "clojure.lang.ArrayChunk@76fe0355"]
+;; `chunk` transforms the temporary buffer `b` into a new chunk.
+(def first-chunk (chunk b)) ; #object[clojure.lang.ArrayChunk 0x4bf443b5 "clojure.lang.ArrayChunk@4bf443b5"]
 
 (comment
-  (chunk-append b 0)) ; (err) Execution error (NullPointerException)
+  ;; a buffer that was used to create a chunk becomes unusable
+  (chunk-append b 0))
+  ; (err) Execution error (NullPointerException)
+  ; (err) Cannot store to object array because "this.buffer" is null
+
+(chunk-cons first-chunk ())       ; (0 1 2)
+(chunk-cons first-chunk '(3 4 5)) ; (0 1 2 3 4 5)
 
 (defn map-chunked [f coll]
   (lazy-seq
@@ -32,9 +41,11 @@
 
 (take 10 (map-chunked inc (range 10000))) ; (1 2 3 4 5 6 7 8 9 10)
 
+;; custom chunked sequence
+;; is = input stream; ib = input buffer; cb = chunk buffer
 (defn byte-seq [^InputStream is size]
   (let [ib (byte-array size)]
-    ((fn step []
+    ((fn step [] ; define `step` and invoke it immediately
        (lazy-seq
         (let [n (.read is ib)]
           (when (not= -1 n)
@@ -48,11 +59,19 @@
     (String. (byte-array (take 20 bs)))))
 ; "A\na\nAA\nAAA\nAachen\nAa"
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (comment
+  ;; with chunking
+  ;; vectors support chunking
   (let [xs (into [] (range 10000))]
     (quick-bench (doall (map inc xs))))
   ;; Execution time mean : 330.650098 µs
 
+  ;; without chunking
+  ;; subvectors don't support chunking
   (let [xs (subvec (into [] (range 10000)) 0 9999)]
     (quick-bench (doall (map inc xs)))))
   ;; Execution time mean : 988.394350 µs
