a27bc37cc7f3b600c564971beb58e161349928f7
Hans-J. Schmid <knock@myopendoor.de>
2025-11-16 21:04:07 +0100

    11.1.3 sorted-map and sorted-map-by

--- 11 - Maps/1 - Creating/3 - sorted-map and sorted-map-by/f/src/f/core.clj
+++ 11 - Maps/1 - Creating/3 - sorted-map and sorted-map-by/f/src/f/core.clj
@@ -2,71 +2,88 @@
   (:require
    [criterium.core :refer [quick-bench]])
   (:import
-   [java.util    TreeMap]
-   [clojure.lang PersistentTreeMap]))
+   [clojure.lang PersistentTreeMap]
+   [java.util    TreeMap]))
 
-(sorted-map :c 3 :b 2 :a 1)  ; {:a 1, :b 2, :c 3}
+(sorted-map :c 3 :b 2 :a 1)        ; {:a 1, :b 2, :c 3}
+(type (sorted-map :c 3 :b 2 :a 1)) ; clojure.lang.PersistentTreeMap
 
 (sorted-map-by
  #(< (:age %1) (:age %2))
  {:age 35} ["J" "K"]
  {:age 13} ["Q" "R"]
  {:age 14} ["T" "V"])
-; {{:age 13} ["Q" "R"], {:age 14} ["T" "V"], {:age 35} ["J" "K"]}
+; {{:age 13} ["Q" "R"], 
+;  {:age 14} ["T" "V"], 
+;  {:age 35} ["J" "K"]
+
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
 
 (defn timed [s]
   (let [t (System/nanoTime)]
-    (println "key" s "created at" t)
+    (println "key" s "created at time" t)
     (with-meta s {:created-at t})))
 
-(def m (sorted-map (timed 'a) 1 (timed 'a) 2))
-; (out) key a created at 8071209171573
-; (out) key a created at 8071209753639
+(def m
+  (sorted-map (timed 'a) 1 (timed 'a) 2))
+; (out) key a created at time 11024338970002
+; (out) key a created at time 11024339568799
 
 m  ; {a 2}
 
-(meta (ffirst m)) ; {:created-at 8071209171573}
+(meta (ffirst m)) ; {:created-at 11024338970002}
+
+;; Priority Queues & A* Algorithm
 
 (sorted-map-by
  compare
- [4.5 "point 2"] [["origin"] 1.5]
- [5.5 "point 3"] [["origin"] 2.5])
-; {[4.5 "point 2"] [["origin"] 1.5], [5.5 "point 3"] [["origin"] 2.5]}
+ [5.5 "point 3"] [["origin"] 2.5]
+ [4.5 "point 2"] [["origin"] 1.5])
+; {[4.5 "point 2"] [["origin"] 1.5], 
+;  [5.5 "point 3"] [["origin"] 2.5])
 
-(def graph
-  {:orig [{:a 1.5 :d 2} 0]
-   :a    [{:orig 1.5 :b 2} 4]
-   :b    [{:a 2 :c 3} 2]
-   :c    [{:b 3 :dest 4} 4]
-   :dest [{:c 4 :e 2} 0]
-   :e    [{:dest 2 :d 3} 2]
-   :d    [{:orig 2 :e 3} 4.5]})
+(def my-graph
+  {:orig [{:a 1.5 :d 2}      0]
+   :a    [{:orig 1.5 :b 2}   4]
+   :b    [{:a 2 :c 3}        2]
+   :c    [{:b 3 :dest 4}     4]
+   :d    [{:orig 2 :e 3}   4.5]
+   :e    [{:dest 2 :d 3}     2]
+   :dest [{:c 4 :e 2}        0]})
 
-(defn discover [node path visited]
+(defn discover [graph node path visited]
   (let [walkable (first (graph node))
-        seen (map last (keys visited))]
+        seen     (map last (keys visited))]
     (reduce dissoc walkable (conj seen (last path)))))
 
-(defn walk [visited dest]
+(defn walk [graph visited dest]
   (loop [visited visited]
-    (let [[[_score node :as current] [path total-distance]] (first visited)]
+    (let [[[_score node :as current] [path total-distance]] (first #p visited)]
       (if (= dest node)
         (conj path dest)
         (recur
          (reduce-kv
           (fn [m neighbour partial-distance]
-            (let [d (+ total-distance partial-distance)
+            (let [d     (+ total-distance partial-distance)
                   score (+ d (last (graph neighbour)))]
               (assoc m [score neighbour] [(conj path node) d])))
           (dissoc visited current)
-          (discover node path visited)))))))
+          (discover graph node path visited)))))))
 
-(defn a* [_graph orig dest]
-  (walk (sorted-map-by compare [0 orig] [[] 0]) dest))
+(defn a* [graph orig dest]
+  (walk graph (sorted-map-by compare [0 orig] [[] 0]) dest))
 
-(a* graph :orig :dest) ; [:orig :d :e :dest]
-(a* graph :d    :c)    ; [:d :e :dest :c]
+(comment
+  (sorted-map-by compare [0 :orig] [[] 0]))
 
+(a* my-graph :orig :dest) ; [:orig :d :e :dest]
+(a* my-graph :d    :c)    ; [:d :e :dest :c]
+
+;; Comparators and Uniqueness of Elements
+
+;; flawed custom comparator(!)
 (sorted-map-by
  #(compare (count %2) (count %1))
  [:a :b] 1 [:a] 2 [:b] 3)
@@ -85,6 +102,9 @@ m  ; {a 2}
 (def flawed-comparator
   #(compare (count %2) (count %1)))
 
+;; The comparator does not work properly because it is used to 
+;; 1. check if a key is in the map 
+;; 2. decide in which relative order it should appear in the resulting sorted-map
 (flawed-comparator [:a] [:x]) ; 0
 
 (def good-comparator
@@ -92,29 +112,31 @@ m  ; {a 2}
 
 (good-comparator [:a] [:x]) ; -23
 
-#_{:clj-kondo/ignore [:redefined-var]}
-(def ordered-by-count
+(def ordered-by-count-correct
   (sorted-map-by
-   #(compare [(count %2) %1] [(count %1) %2])
+   good-comparator
    [:a :b] 1 [:a] 2 [:b] 3))
 
-(assoc ordered-by-count [:x] 4) ; {[:a :b] 1, [:a] 2, [:b] 3, [:x] 4}
-
-(dissoc ordered-by-count [:x])  ; {[:a :b] 1, [:a] 2, [:b] 3}
+(assoc  ordered-by-count-correct [:x] 4)              ; {[:a :b] 1, [:a] 2, [:b] 3, [:x] 4}
+(dissoc ordered-by-count-correct [:x])                ; {[:a :b] 1, [:a] 2, [:b] 3}
+(dissoc (assoc ordered-by-count-correct [:x] 4) [:x]) ; {[:a :b] 1, [:a] 2, [:b] 3}
 
 (comment
+  ;; different ways to create a sorted-map with 1000 keys
+
   (let [pairs (into [] (range 2e3))]                        ; (out) Execution time mean : 1.279825 ms
     (quick-bench (apply sorted-map pairs)))
 
   (let [pairs (into [] (map-indexed vector (range 1e3)))]   ; (out) Execution time mean : 1.164903 ms
     (quick-bench (into (sorted-map) pairs)))
 
+  ;; mutable Java version
   (let [m (apply hash-map (into [] (range 2e3)))]           ; (out) Execution time mean : 253.548921 µs
     (quick-bench (TreeMap. m)))
 
   (let [m (apply sorted-map (range 10))]                    ; (out) Execution time mean : 97.977706 ns
     (quick-bench (first m)))
 
+  ;; using Java interop
   (let [m (apply sorted-map (range 10))]                    ; (out) Execution time mean : 10.391083 ns
     (quick-bench (.min ^PersistentTreeMap m))))
-
--- 11 - Maps/1 - Creating/4 - create-struct, defstruct, struct-map, struct and accessor/f/src/f/core.clj
+++ 11 - Maps/1 - Creating/4 - create-struct, defstruct, struct-map, struct and accessor/f/src/f/core.clj
@@ -3,49 +3,94 @@
    [clojure.string :refer [split split-lines]]
    [criterium.core :refer [quick-bench]]))
 
+;; structs are not obsolete, but superseded by records
+
+;; A struct is much simpler than the more powerful defrecord.
+;; One fundamental difference is that create-struct creates an anonymous struct definition,
+;; while defrecord creates a Java class as a side effect. 
+;; defrecord generates a class by design: this allows powerful features such as inheritance of record types.
+
+;; create-struct: create a struct definition
+;; defstruct: create a named struct definition
+;; struct: create a struct instance from a struct definition
+;; struct-map: create a struct instance from a struct defintion
+;; accessor: create an accessor function from a struct defintion for a struct field
+;;           optimizes frequent access by skipping the typical hash-based access by a faster array-index lookup
+
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+;; structs are map-like types with a minimal set of required keys
+
+;; comparing struct definitions
 (= (create-struct :x :y)
    (create-struct :x :y))
 ; false
 
+;; comparing struct instances (two instances with same keys and values are the same)
 (= (struct (create-struct :x :y) 1 2)
    (struct (create-struct :x :y) 1 2))
 ; true
 
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defstruct point-2d :x :y)
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defstruct point-3d :x :y :z)
 
-(= (assoc (struct point-2d 1 2) :z 3)
-   (struct point-3d 1 2 3))
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+;; adding the missing key makes them equal
+(= (struct point-3d 1 2 3)
+   (assoc (struct point-2d 1 2) :z 3))
 ; true
 
+;; struct can be anonymous
 (struct (create-struct :a :b :c) 1 2 3)
 ; {:a 1, :b 2, :c 3}
 
+;; `defrecord` needs a name and generates a class
 (type (defrecord abc [a b c])) ; java.lang.Class
 
+;; new record instance
 (abc. 1 2 3) ; {:a 1, :b 2, :c 3}
-; #user.abc{:a 1, :b 2, :c 3}
 
-(def waypoints "https://tinyurl.com/station-locs")
-(def lines (-> waypoints slurp split-lines))
-; ["VHF  0.000000 0.000000 ABI "
-;  "VHF  32.464722 13.170000 ABU "
-;  "VHF  -28.570555 16.533890 ABV "
-;  "VHF  5.633333 -0.155000 ACC "
-;  "VHF  5.277194 -3.919306 AD  "
-; …]
+;; Waypoint (contains the coordinates of a point of interest on Earth)
 
-(def waypoints
-  (let [sdef (create-struct :type :lat :lon :id)]
-    (transduce
-     (comp
-      (map #(split % #"\s+"))
-      (map #(apply struct sdef %)))
-     conj
-     lines)))
+(def waypoints-url "https://tinyurl.com/station-locs")
 
-(first waypoints) ; {:type "VHF", :lat "0.000000", :lon "0.000000", :id "ABI"}
+(comment
+  ;; takes a while
+  (def lines (->
+              waypoints-url
+              slurp
+              split-lines))
+  ; ["VHF  0.000000 0.000000 ABI "
+  ;  "VHF  32.464722 13.170000 ABU "
+  ;  "VHF  -28.570555 16.533890 ABV "
+  ;  "VHF  5.633333 -0.155000 ACC "
+  ;  "VHF  5.277194 -3.919306 AD  "
+  ; …]
 
+  (count lines) ; 42909
+
+  (def waypoints
+    (let [sdef (create-struct :type :lat :lon :id)]
+      (transduce
+       (comp
+        (map #(split % #"\s+"))       ; [["VHF" "0.000000" "0.000000" "ABI"] ["VHF" "32.464722" "13.170000" "ABU"] ["VHF" "-28.570555" "16.533890" "ABV"] ["VHF" "5.633333" "-0.155000" "ACC"]…]
+        (map #(apply struct sdef %)))
+       conj
+       lines)))
+  ; [{:type "VHF", :lat "0.000000",   :lon "0.000000",  :id "ABI"}
+  ;  {:type "VHF", :lat "32.464722",  :lon "13.170000", :id "ABU"}
+  ;  {:type "VHF", :lat "-28.570555", :lon "16.533890", :id "ABV"}
+  ;  {:type "VHF", :lat "5.633333",   :lon "-0.155000", :id "ACC"}
+  ;  {:type "VHF", :lat "5.277194",   :lon "-3.919306", :id "AD"
+
+  (count waypoints)) ; 42909
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defstruct waypoint :type :lat :lon :id)
 
 (def coordinates
@@ -53,57 +98,88 @@
    {:alt 312 :lon "10.04883" :id "FFA" :type "XFV"}
    {:temp 78.3 :lat "23.7611" :id "XJP"}])
 
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+;; create waypoints from a list of hash-maps
 (defn to-waypoints [coords]
-  (map #(apply struct-map waypoint (mapcat identity %)) coords))
+  (map
+   #(apply struct-map waypoint (mapcat identity %))
+   coords))
 
+(comment
+  (mapcat identity {:alt 150 :lat "18.3112" :lon "3.1314" :id "XVA"}))
+  ; (:alt 150 :lat "18.3112" :lon "3.1314" :id "XVA")
+
+;; creates a list of waypoint structs
 (to-waypoints coordinates)
-; ({:type nil, :lat "18.3112", :lon "3.1314", :id "XVA", :alt 150}
-;  {:type "XFV", :lat nil, :lon "10.04883", :id "FFA", :alt 312}
-;  {:type nil, :lat "23.7611", :lon nil, :id "XJP", :temp 78.3})
+; ({:type nil,   :lat "18.3112", :lon "3.1314",   :id "XVA", :alt 150}
+;  {:type "XFV", :lat nil,       :lon "10.04883", :id "FFA", :alt 312}
+;  {:type nil,   :lat "23.7611", :lon nil,        :id "XJP", :temp 78.3})
 
+;; accessors for each key in a waypoint
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (def k-type (accessor waypoint :type))
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (def k-lat  (accessor waypoint :lat))
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+#_{:clojure-lsp/ignore [:clojure-lsp/unused-public-var]}
 (def k-lon  (accessor waypoint :lon))
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (def k-id   (accessor waypoint :id))
 
-#_{:clj-kondo/ignore [:redefined-var]}
-(def waypoints (to-waypoints coordinates))
-; ({:type nil, :lat "18.3112", :lon "3.1314", :id "XVA", :alt 150}
-;  {:type "XFV", :lat nil, :lon "10.04883", :id "FFA", :alt 312}
-;  {:type nil, :lat "23.7611", :lon nil, :id "XJP", :temp 78.3})
-
-(map k-id waypoints)
+(map k-id (to-waypoints coordinates))
 ; ("XVA" "FFA" "XJP")
 
-(def waypoints "https://tinyurl.com/station-locs")
-(def lines (->> waypoints slurp split-lines (map #(split % #"\s+"))))
-(last lines)
-;; ["ARP" "44.244823" "-84.179802" "Y31"] ; <1>
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defstruct w-struct :type :lat :lon :id) ; <2>
+(comment
+  (def lines
+    (->> waypoints-url
+         slurp
+         split-lines
+         (map #(split % #"\s+")))))
+  ; (["VHF" "0.000000" "0.000000" "ABI"]
+  ;  ["VHF" "32.464722" "13.170000" "ABU"]
+  ;  ["VHF" "-28.570555" "16.533890" "ABV"]
+  ;  ["VHF" "5.633333" "-0.155000" "ACC"]
+  ;  ["VHF" "5.277194" "-3.919306" "AD"]
+  ; …)
+
+#_{:clj-kondo/ignore [:unresolved-symbol]}
+(defstruct w-struct :type :lat :lon :id)
 (defrecord w-record [type lat lon id])
-(defn w-map [type lat lon id] {:type type :lat lat :lon lon :id id})
+
+(defn w-map  [type lat lon id] {:type type :lat lat :lon lon :id id})
 (defn w-hmap [type lat lon id] (hash-map :type type :lat lat :lon lon :id id))
 
-(def points-struct (doall (map #(apply struct w-struct %) lines)))
-(def points-record (doall (map #(apply ->w-record %) lines)))
-(def points-map    (doall (map #(apply w-map %) lines)))
-(def points-hmap   (doall (map #(apply w-hmap %) lines)))
+(comment
+  #_{:clj-kondo/ignore [:unresolved-symbol]}
+  (def points-struct (doall (map #(apply struct w-struct %) lines)))
+  (def points-record (doall (map #(apply ->w-record %)      lines)))
+  (def points-map    (doall (map #(apply w-map %)           lines)))
+  (def points-hmap   (doall (map #(apply w-hmap %)          lines))))
 
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (def id (accessor w-struct :id))
 
 (comment
+  #_{:clj-kondo/ignore [:unresolved-symbol]}
   (quick-bench (doall (map #(apply struct w-struct %) lines))) ; (out) Execution time mean : 16.751639 ms
-  (quick-bench (doall (map #(apply ->w-record %) lines)))      ; (out) Execution time mean : 15.579792 ms
-  (quick-bench (doall (map #(apply w-map %) lines)))           ; (out) Execution time mean : 17.456291 ms
-  (quick-bench (doall (map #(apply w-hmap %) lines)))          ; (out) Execution time mean : 52.904692 ms
+  (quick-bench (doall (map #(apply ->w-record %)      lines))) ; (out) Execution time mean : 15.579792 ms
+  (quick-bench (doall (map #(apply w-map %)           lines))) ; (out) Execution time mean : 17.456291 ms
+  (quick-bench (doall (map #(apply w-hmap %)          lines))) ; (out) Execution time mean : 52.904692 ms
 
-  (let [w (first points-struct)] (quick-bench (id  w)))  ; (out) Execution time mean : 5.237529 ns
-  (let [w (first points-struct)] (quick-bench (:id  w))) ; (out) Execution time mean : 25.221455 ns
-  (let [w (first points-record)] (quick-bench (:id w)))  ; (out) Execution time mean : 15.107096 ns
-  (let [w (first points-map)]    (quick-bench (:id w)))  ; (out) Execution time mean : 24.093760 ns
-  (let [w (first points-hmap)]   (quick-bench (:id w)))  ; (out) Execution time mean : 48.636786 ns
-
-  (let [^user.w-record w (first points-record)] (quick-bench (.id w))))
-;; Execution time mean : 3.612035 ns
+  (let [w           (first points-struct)] (quick-bench  (id w)))  ; (out) Execution time mean :  5.237529 ns
+  (let [w           (first points-struct)] (quick-bench (:id w)))  ; (out) Execution time mean : 25.221455 ns
+  (let [w           (first points-record)] (quick-bench (:id w)))  ; (out) Execution time mean : 15.107096 ns
+  (let [w           (first points-map)]    (quick-bench (:id w)))  ; (out) Execution time mean : 24.093760 ns
+  (let [w           (first points-hmap)]   (quick-bench (:id w)))  ; (out) Execution time mean : 48.636786 ns
+  (let [^w-record w (first points-record)] (quick-bench (.id w)))) ; (out) Execution time mean : 0.093315 ns
 
