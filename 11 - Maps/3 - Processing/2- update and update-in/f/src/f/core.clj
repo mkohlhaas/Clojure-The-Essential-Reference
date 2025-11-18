d9f48aeabbd2f8404b1e912443335082db5cdbda
Hans-J. Schmid <knock@myopendoor.de>
2025-11-18 22:25:23 +0100

    11.3.2 update and update-in

--- 11 - Maps/3 - Processing/2- update and update-in/f/src/f/core.clj
+++ 11 - Maps/3 - Processing/2- update and update-in/f/src/f/core.clj
@@ -1,25 +1,35 @@
 (ns f.core)
 
+;; `update` and `update-in` accept a function of the old value into the new one
+
 (update {:a 1 :b 2} :b inc)  ; {:a 1, :b 3}
 
 (update-in {:a 1 :b {:c 2}} [:b :c] inc)  ; {:a 1, :b {:c 3}}
 
-(def words
-  {"morning" 2 "bye" 1 "hi" 5 "gday" 2})
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
 
 (defn insert-word [w words]
   (update words w (fnil inc 0)))
 
-(insert-word "hello" words) ; {"morning" 2, "bye" 1, "hi" 5, "gday" 2, "hello" 1}
+(let [words {"morning" 2, "bye" 1, "hi" 5, "gday" 2}]
+  (insert-word "hello" words))                         ; {"morning" 2, "bye" 1, "hi" 5, "gday" 2, "hello" 1}
 
-(update [:a :b :c] 3 (fnil keyword "d"))  ; [:a :b :c :d]
-;; [:a :b :c :d]
+;; update on vectors allows the addition of a new element at the tail
+(update [:a :b :c] 3 (fnil keyword "d")) ; [:a :b :c :d]
+(update [:a :b :c] 2 (fnil keyword "d")) ; [:a :b :c]
 
 (comment
-  (update [:a :b :c] 4 (fnil keyword "d")))
+  (keyword "d") ; :d
+
+  ;; not the tail
+  (update [:a :b :c] 4 (fnil keyword "d"))) ; nil
   ; (err) Execution error (IndexOutOfBoundsException)
 
-(def products
+;; decrease number of item when product is sold
+
+(def products-db
   {"A011" {:in-stock 10
            :name "Samsung G5"}
    "B032" {:in-stock 4
@@ -33,24 +43,34 @@
    [id :in-stock]
    (fnil dec 2)))
 
+;; get number of items of sold product "B032"
 (get-in
- (sale products "B032")
+ (sale products-db "B032") ; {"A011" {:in-stock 10, :name "Samsung G5"}, "B032" {:in-stock 3, :name "Apple iPhone"}, "AE33" {:in-stock 13, :name "Motorola N1"}}
  ["B032" :in-stock])
 ; 3
 
-#_{:clj-kondo/ignore [:redefined-var]}
-(def products
-  (atom {"A011" {:in-stock 10
-                 :name "Samsung G5"}
-         "B032" {:in-stock 4
-                 :name "Apple iPhone"}
-         "AE33" {:in-stock 13
-                 :name "Motorola N1"}}))
+;; Compare and Swap (CAS) transactions
+;; `update` (but also `assoc` and their *-in variants) are often seen in conjunction with `swap!`
 
-(defn total-products [products]
+(def products-atom-db
+  (atom
+   {"A011" {:in-stock 10
+            :name "Samsung G5"}
+    "B032" {:in-stock 4
+            :name "Apple iPhone"}
+    "AE33" {:in-stock 13
+            :name "Motorola N1"}}))
+
+(defn num-total-products [products]
   (reduce + (map :in-stock (vals products))))
 
-(total-products @products) ; 27
+(comment
+  (vals @products-atom-db))
+  ; ({:in-stock 10, :name "Samsung G5"}
+  ;  {:in-stock  4, :name "Apple iPhone"}
+  ;  {:in-stock 13, :name "Motorola N1"})
+
+(num-total-products @products-atom-db) ; 27
 
 (defn sale! [products id]
   (swap! products
@@ -59,9 +79,15 @@
          (fnil dec 2))
   products)
 
-(defn sale-simulation! [ids]
+(defn sale-simulation! [products ids]
   (dorun (pmap (partial sale! products) ids)))
 
-(sale-simulation! ["B032" "AE33" "A011" "A011" "AE33" "B032"]) ; nil
+;; selling 6 items
+(sale-simulation! products-atom-db ["B032" "AE33" "A011" "A011" "AE33" "B032"])
 
-(total-products @products) ; 21
+@products-atom-db
+; {"A011" {:in-stock  8, :name "Samsung G5"},
+;  "B032" {:in-stock  2, :name "Apple iPhone"},
+;  "AE33" {:in-stock 11, :name "Motorola N1"}}
+
+(num-total-products @products-atom-db) ; 21
