23634615bf45b20485d42131d1433f994e992391
Hans-J. Schmid <knock@myopendoor.de>
2025-11-21 23:34:27 +0100

    12.10 subvec

--- 12 - Vectors/7 - subvec/f/src/f/core.clj
+++ 12 - Vectors/7 - subvec/f/src/f/core.clj
@@ -2,6 +2,8 @@
   (:require
    [criterium.core :refer [quick-bench]]))
 
+;; the result of `subvec` is an independent view of a range of elements contained in the input vector
+
 (subvec [1 2 3 4] 1 3) ; [2 3]
 
 (def subv (subvec (vector-of :int 1 2 3) 1)) ; [2 3]
@@ -12,36 +14,48 @@
   (conj subv nil))
   ; (err) Execution error (NullPointerException)
 
+;; end is optional
 (subvec [1 2 3 4] 1) ; [2 3 4]
 
+;; ;;;;;;;;
+;; Examples
+;; ;;;;;;;;
+
+;; efficient solution to remove an element
 (defn remove-at [v idx]
   (into (subvec v 0 idx)
         (subvec v (inc idx) (count v))))
 
 (remove-at [0 1 2 3 4 5] 3) ; [0 1 2 4 5]
 
+;; norm of a vector
 (defn norm-1 [v]
   (loop [v   v
          res 0.]
     (if (= 0 (count v))
       (Math/sqrt res)
-      (recur (subvec v 1)
+      (recur (subvec v 1) ; `subvec` can be used recursively (like first/rest)
              (+ res (Math/pow (nth v 0) 2))))))
 
 (norm-1 [-2 1]) ; 2.23606797749979
 
-(defn- merge-vectors [v1-initial v2-initial cmp]
-  (loop [result []
-         v1     v1-initial
-         v2     v2-initial]
+(comment
+  ;; return head of vector
+  (nth [1 2 3] 0)) ; 1
+
+;; merge operation of the merge sort algorithm (v1 and v2 are sorted)
+(defn- merge-vectors [v1 v2 cmp]
+  (loop [res []
+         v1  v1
+         v2  v2]
     (cond
-      (empty? v1) (into result v2)
-      (empty? v2) (into result v1)
+      (empty? v1) (into res v2)
+      (empty? v2) (into res v1)
       :else (let [[v1-head & v1-tail] v1
                   [v2-head & v2-tail] v2]
               (if (cmp v1-head v2-head)
-                (recur (conj result v1-head) v1-tail v2)
-                (recur (conj result v2-head) v1 v2-tail))))))
+                (recur (conj res v1-head) v1-tail v2)
+                (recur (conj res v2-head) v1 v2-tail))))))
 
 (defn merge-sort
   ([v]
@@ -54,12 +68,16 @@
            v2    (subvec v split (count v))]
        (merge-vectors (merge-sort v1 cmp) (merge-sort v2 cmp) cmp)))))
 
-(merge-sort [2 1 5 0 3]) ; [0 1 2 3 5]
-
+(merge-sort [2 1 5 0 3])                                        ; [0 1 2 3 5]
 (merge-sort [[2 :b] [2 :a] [1 :c]] #(<= (first %1) (first %2))) ; [[1 :c] [2 :b] [2 :a]]
 
-(defn norm-2 [v] ; <1>
-  (loop [v v
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Performance Considerations
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; index-based with `subvec`
+(defn norm-2 [v]
+  (loop [v   v
          res 0.
          idx (dec (count v))]
     (if (< idx 0)
@@ -68,12 +86,8 @@
              (+ res (Math/pow (peek v) 2))
              (dec idx)))))
 
-(comment
-  (let [v (vec (range 1000))]
-    (quick-bench (norm-2 v))))
-  ; (out) Execution time mean : 112.584725 µs
-
-(defn norm-idx [v] ; <3>
+;; index-based without `subvec`
+(defn norm-idx [v]
   (loop [idx (dec (count v))
          res 0.]
     (if (< idx 0)
@@ -82,14 +96,17 @@
              (+ res (Math/pow (nth v idx) 2))))))
 
 (comment
-  (let [v (vec (range 1000))]
+  (let [v (vec (range 1000))]     ; (out) Execution time mean : 112.584725 µs
+    (quick-bench (norm-2 v)))
+
+  (let [v (vec (range 1000))]     ; (out) Execution time mean :  19.212695 µs
     (quick-bench (norm-idx v))))
-  ; (out)              Execution time mean : 19.212695 µs
 
 (defn bigv [n]
   (vec (range n)))
 
 (comment
+  ;; The generated sub-vector maintains a reference to the original vector though, which can prevent elements being garbaged collected. 
   (let [v1 (subvec (bigv 1e9) 0 5)
         v2 (subvec (bigv 1e9) 5 10)]
     (into v1 v2))
@@ -97,7 +114,7 @@
 ; (err) Java heap space
 
   ;; should not run out of memory
-  (let [v1 (into [] (subvec (bigv 1e9) 0 5))
+  (let [v1 (into [] (subvec (bigv 1e9) 0 5))   ; each subvector is transferred into a new vector instance, so their inner vector reference can be garbage collected
         v2 (into [] (subvec (bigv 1e9) 5 10))]
     (into v1 v2)))
   ; (err) Execution error (OutOfMemoryError) at java.lang.Long/valueOf (Long.java:1000).
