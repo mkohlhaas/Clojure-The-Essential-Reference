ce6fc4421f2433e1c265d9b154fda939251306fc
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 02:20:13 +0100

    16.8 ns-map and ns-unmap

--- 16 - Vars and Namespaces/08 - ns-map and ns-unmap/f/src/f/core.clj
+++ 16 - Vars and Namespaces/08 - ns-map and ns-unmap/f/src/f/core.clj
@@ -1,6 +1,37 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(take 10 (ns-map 'f.core))
+; ([primitives-classnames #'clojure.core/primitives-classnames]
+;  [+' #'clojure.core/+']
+;  [Enum java.lang.Enum]
+;  [decimal? #'clojure.core/decimal?]
+;  [restart-agent #'clojure.core/restart-agent]
+;  [sort-by #'clojure.core/sort-by]
+;  [macroexpand #'clojure.core/macroexpand]
+;  [ensure #'clojure.core/ensure]
+;  [chunk-first #'clojure.core/chunk-first]
+;  [eduction #'clojure.core/eduction])
+
+;; (ns myns (:require [clojure.pprint :refer [pprint]]))
+
+;; (pprint (ns-map 'myns))
+;; {primitives-classnames #'clojure.core/primitives-classnames,
+;;  +' #'clojure.core/+',
+;;  Enum java.lang.Enum,
+;;  decimal? #'clojure.core/decimal?,
+;;  restart-agent #'clojure.core/restart-agent,
+;;  sort-by #'clojure.core/sort-by,
+;; [...]
+
+;; (distinct (map #(map type %) (ns-map 'myns)))
+;; ((clojure.lang.Symbol clojure.lang.Var)
+;;  (clojure.lang.Symbol java.lang.Class))
+
+;; (ns-unmap 'myns '+)
+;; nil
+
+(+ 1 1)
+;; Unable to resolve symbol: +
+
+(refer 'clojure.core :only ['+])
+(+ 1 1) ; 2
