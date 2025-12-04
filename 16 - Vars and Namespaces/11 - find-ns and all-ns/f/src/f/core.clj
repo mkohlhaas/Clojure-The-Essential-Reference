5db9d6fc838e7d4d547e5c5c57c925ed8bf2be85
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 16:08:02 +0100

    16.11 find-ns and all-ns

--- 16 - Vars and Namespaces/11 - find-ns and all-ns/f/src/f/core.clj
+++ 16 - Vars and Namespaces/11 - find-ns and all-ns/f/src/f/core.clj
@@ -1,6 +1,18 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(take 10 (all-ns))
+; (#object[clojure.lang.Namespace 0x3c92ce0 "clojure.spec.gen.alpha"]
+;  #object[clojure.lang.Namespace 0x71306cb9 "nrepl.middleware.interruptible-eval"]
+;  #object[clojure.lang.Namespace 0x6455ddbd "cider.nrepl.pprint"]
+;  #object[clojure.lang.Namespace 0x6053875c "clojure.stacktrace"]
+;  #object[clojure.lang.Namespace 0x7c0822b1 "clojure.core.rrb-vector.transients"]
+;  #object[clojure.lang.Namespace 0x7b0e6290 "clojure.uuid"]
+;  #object[clojure.lang.Namespace 0x31fce079 "clojure.main"]
+;  #object[clojure.lang.Namespace 0x4de9e87d "user"]
+;  #object[clojure.lang.Namespace 0x704956db "clojure.test"]
+;  #object[clojure.lang.Namespace 0x785bb0a0 "clojure.data"])
+
+(ns-name (first (all-ns))) ; clojure.spec.gen.alpha
+
+(find-ns 'clojure.edn) ; #object[clojure.lang.Namespace 0x2cdad749 "clojure.edn"]
+(find-ns 'no-ns)       ; nil
