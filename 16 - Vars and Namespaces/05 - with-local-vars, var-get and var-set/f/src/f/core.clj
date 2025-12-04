f17f7011afbe95f840a203146339971cc3872342
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 01:46:01 +0100

    16.5 with-local-vars, var-get and var-set

--- 16 - Vars and Namespaces/05 - with-local-vars, var-get and var-set/f/src/f/core.clj
+++ 16 - Vars and Namespaces/05 - with-local-vars, var-get and var-set/f/src/f/core.clj
@@ -1,6 +1,17 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; - `with-local-vars` creates a thread local scope where vars can be created and mutated
+;; -  along with `var-get` and `var-set`, `with-local-vars` allows an imperative, thread-safe style of programming
+;; - `with-local-vars` is rarely seen in real-life Clojure code (because ot its imperative style)
+
+(defn ++ [v]
+  (var-set v (inc (var-get v))))
+
+(defn count-even [xs]
+  (with-local-vars [a 0]
+    (doseq [x xs]
+      (when (zero? (rem x 2))
+        (++ a)))
+    @a))
+
+(count-even (range 10)) ; 5
