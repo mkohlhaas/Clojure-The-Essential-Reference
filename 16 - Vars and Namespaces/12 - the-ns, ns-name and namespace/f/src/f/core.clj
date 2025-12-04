5cbc212cf002cb01c61f8bc34be3505a901bc463
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 16:15:02 +0100

    16.12 the-ns, ns-name and namespace

--- 16 - Vars and Namespaces/12 - the-ns, ns-name and namespace/f/src/f/core.clj
+++ 16 - Vars and Namespaces/12 - the-ns, ns-name and namespace/f/src/f/core.clj
@@ -1,6 +1,15 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(comment
+  (the-ns 'notavail)) ; (err) No namespace: notavail found
+(the-ns 'clojure.edn) ; #object[clojure.lang.Namespace 0x46f2cd97 "clojure.edn"]
+(the-ns *ns*)         ; #object[clojure.lang.Namespace 0x5a3a707c "f.core"]
+
+;; NOTE: Works only in the REPL!!!
+;;
+;; (ns com.package.myns)
+;; (ns-name *ns*) ; com.package.myns
+
+(namespace :notcreateyet/a) ; "notcreateyet"
+(namespace ::a)             ; "f.core" (implicit full qualification to the current namespace is possible by using the double colon notation)
+(namespace 'alsosymbols/s)  ; "alsosymbols"
