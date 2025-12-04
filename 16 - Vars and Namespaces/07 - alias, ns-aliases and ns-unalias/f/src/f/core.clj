f2e17ba1b644805d00858afccac57d5ec31c5b5f
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 02:17:01 +0100

    16.7 alias, ns-aliases and ns-unalias

--- 16 - Vars and Namespaces/07 - alias, ns-aliases and ns-unalias/f/src/f/core.clj
+++ 16 - Vars and Namespaces/07 - alias, ns-aliases and ns-unalias/f/src/f/core.clj
@@ -1,6 +1,28 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(create-ns 'com.web.tired-of-typing-this.myns)  ; #object[clojure.lang.Namespace 0x152d3c36 "com.web.tired-of-typing-this.myns"]
+(ns-aliases 'com.web.tired-of-typing-this.myns) ; {} (is empty after creation)
+
+;; 2.clj
+
+(intern 'com.web.tired-of-typing-this.myns 'myvar 0) ; #'com.web.tired-of-typing-this.myns/myvar
+#_{:clj-kondo/ignore [:unresolved-namespace]}
+com.web.tired-of-typing-this.myns/myvar              ; 0
+
+(alias 'myns 'com.web.tired-of-typing-this.myns)
+
+(ns-aliases *ns*) ; {myns #object[clojure.lang.Namespace 0x152d3c36 "com.web.tired-of-typing-this.myns"]}
+
+;; much shorter now
+myns/myvar ; 0
+
+;; Same effect as `:as` in `ns`:
+;; (ns anotherns
+;;   (:require [clojure.set :as s])
+
+;; (ns-aliases 'anotherns) ; {s #object[clojure.lang.Namespace 0x5d1fa08b "clojure.set"]}
+
+;; remove aliases:
+;; (ns-aliases 'anotherns)    ; {s #object[clojure.lang.Namespace 0x5d1fa08b "clojure.set"]}
+;; (ns-unalias 'anotherns 's)
+;; (ns-aliases 'anotherns)    ; {}
