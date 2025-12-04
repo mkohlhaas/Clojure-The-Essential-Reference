30cc0675150677e3514062054caea3733c6f7819
Hans-J. Schmid <knock@myopendoor.de>
2025-12-04 16:04:06 +0100

    16.10 refer, refer-clojure, require, loaded-libs, use, import

--- 16 - Vars and Namespaces/10 - refer, refer-clojure, require, loaded-libs, use, import/f/src/f/core.clj
+++ 16 - Vars and Namespaces/10 - refer, refer-clojure, require, loaded-libs, use, import/f/src/f/core.clj
@@ -1,6 +1,114 @@
 (ns f.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; ;;;;;
+;; refer
+;; ;;;;;
+
+(defn clean-ns [ns]
+  (create-ns ns)
+  (let [keys (keys (ns-map ns))]
+    (doseq [key keys]
+      (ns-unmap ns key)))
+  (ns-map ns))
+
+(clean-ns 'myns) ; {}
+
+(binding [*ns* (the-ns 'myns)] ; `refer` uses only current namespace (in *ns*)
+  (refer 'clojure.core
+         :only   ['+ '-]
+         :rename {'+ 'plus '- 'minus}))
+
+(ns-map 'myns)
+; {minus #'clojure.core/-,
+;  plus  #'clojure.core/+)
+
+(clean-ns 'myns) ; {}
+
+;; the same:
+;; (binding [*ns* (the-ns 'myns)]
+;;   (refer-clojure
+;;    :only   ['+ '-]
+;;    :rename {'+ 'plus '- 'minus}))
+
+;; ;;;;;;;
+;; require
+;; ;;;;;;;
+
+;; NOTE: These examples work only in the REPL!!!
+
+;; (contains? (set (map ns-name (all-ns))) 'clojure.set)
+;; false
+
+;; (require 'clojure.set)
+;;
+;; (contains? (set (map ns-name (all-ns))) 'clojure.set)
+;; ;; true
+;;
+;; (def libs (loaded-libs))
+;;
+;; (take 10 libs)
+;; ; (arrangement.core
+;; ;  clj-stacktrace.core
+;; ;  clojure.core.protocols
+;; ;  clojure.core.reducers
+;; ;  clojure.core.rrb-vector
+;; ;  clojure.core.rrb-vector.fork-join
+;; ;  clojure.core.rrb-vector.interop
+;; ;  clojure.core.rrb-vector.nodes
+;; ;  clojure.core.rrb-vector.parameters
+;; ;  clojure.core.rrb-vector.protocols)
+;;
+;; (require '[clojure.data :refer [diff]])
+;;
+;; (def nss (set (map ns-name (all-ns))))
+;; (take 2 (diff libs nss))
+;; ; (nil 
+;; ;  #{cider.nrepl.pprint reply.exports clojure.core myns})
+;;
+;; ;; 5.clj
+;;
+;; (create-ns 'test-require)
+;; (require 'test-require)
+;; ;; Could not locate test_require__init.class or test_require.clj on classpath.
+;;
+;; ;; 6.clj
+;;
+;; (ns myns)
+;;
+;; (require
+;;  '[clojure.set
+;;    :as se
+;;    :refer [union]]
+;;  '[clojure.string
+;;    :as st
+;;    :refer :all])
+;; ;; WARNING: reverse already refers to: #'clojure.core/reverse
+;; ;; WARNING: replace already refers to: #'clojure.core/replace
+;;
+;; ;; 7.clj
+;;
+;; (ns myns)
+;;
+;; (use '[clojure.java.io
+;;        :only [reader file]
+;;        :rename {reader r}]
+;;      :verbose
+;;      :reload-all)
+;;
+;; ;; (load "/clojure/java/io")
+;; ;; (in-ns 'myns)
+;; ;; (refer 'clojure.java.io :only '[reader file] :rename '{reader r})
+;;
+;; ;; 8.clj
+;;
+;; (ns user)
+;; (clean-ns 'myns)
+;; ;; {}
+;;
+;; (binding [*ns* (the-ns 'myns)]
+;;   (import '[java.util ArrayList HashMap]))
+;;
+;; (ns-imports 'myns)
+;; ;; {HashMap java.util.HashMap
+;; ;;  ArrayList java.util.ArrayList}
+;;
